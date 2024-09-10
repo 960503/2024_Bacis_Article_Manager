a1da3dcc5e97d889e502f4fc2fc086b824bcdf65
960503 <hanewnji@gmail.com>
2024-09-11 02:10:52 +0900

    article write

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,34 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
+		int lasteArticleId = 1;
+
 		while (true) {
 
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
 
-			System.out.println("입력된 명령어 : " + cmd);
+			
+			if (cmd.equals("article write")) {
+				
+				System.out.printf("제목 : ");
+				sc.nextLine();
+				
+				System.out.printf("내용 : ");
+				sc.nextLine();
+				
+				System.out.printf("%d번 글이 생성되었습니다.\n", lasteArticleId);
+				lasteArticleId++;
+			}
+			
+			if (cmd.equals("article list")) {
+				
+				System.out.println("게시글이 없습니다.");
+			}
+			
+			if (cmd.equals("")) {
+				System.out.println("명령어를 입력해주세요.");
+			}
 
 			if (cmd.equals("exit")) {
 				break;
