21575aa04d22d33c21b5800968c105c79de03c06
960503 <hanewnji@gmail.com>
2024-09-11 02:16:47 +0900

    존재하지 않는 명령어

--- src/Main.java
+++ src/Main.java
@@ -14,26 +14,25 @@ public class Main {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
 
-			
 			if (cmd.equals("article write")) {
-				
+
 				System.out.printf("제목 : ");
 				sc.nextLine();
-				
+
 				System.out.printf("내용 : ");
 				sc.nextLine();
-				
+
 				System.out.printf("%d번 글이 생성되었습니다.\n", lasteArticleId);
 				lasteArticleId++;
-			}
-			
-			if (cmd.equals("article list")) {
 				
+			} else if (cmd.equals("article list")) {
+
 				System.out.println("게시글이 없습니다.");
-			}
+				
+			} else {
 			
-			if (cmd.equals("")) {
 				System.out.println("명령어를 입력해주세요.");
+			
 			}
 
 			if (cmd.equals("exit")) {
