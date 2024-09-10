ebebfe2517650212bc9faf949b0f5272d5758ec6
960503 <hanewnji@gmail.com>
2024-09-11 02:30:18 +0900

    명령어를 입력해주세요

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,14 @@ public class Main {
 		while (true) {
 
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
+
+			if (cmd.length() == 0) {
+
+				System.out.println("명령어를 입력해주세요.");
+				continue;
+
+			}
 
 			if (cmd.equals("article write")) {
 
@@ -24,15 +31,15 @@ public class Main {
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", lasteArticleId);
 				lasteArticleId++;
-				
+
 			} else if (cmd.equals("article list")) {
 
 				System.out.println("게시글이 없습니다.");
-				
+
 			} else {
-			
-				System.out.println("명령어를 입력해주세요.");
-			
+
+				System.out.println("존재하지 않는 명령어입니다.");
+
 			}
 
 			if (cmd.equals("exit")) {
